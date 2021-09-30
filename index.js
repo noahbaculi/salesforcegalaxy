2499d9a912ffbf56de9dcb9ddc3fb95fd4bab6af
Noah <nbaculi@hotmail.com>
2021-09-30 12:27:26 -0700

    Removed mouse hover detection for sound effects.

--- index.js
+++ index.js
@@ -151,32 +151,3 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
-
-// 
-// HTML links hover sound effect
-
-const gridElements = document.getElementsByClassName("grid")[0];
-const links = Array.prototype.slice.call(gridElements.children);
-// console.log(links);
-
-links.forEach((link) => {
-  console.log(link.localName);
-  if (link.localName === "a") {
-    const hoverSound = new Audio("data/spaceboom.wav"); // buffers automatically when created
-    link.addEventListener(
-      "mouseenter",
-      function (event) {
-        console.log("mouseenter");
-        hoverSound.play();
-      },
-      false
-    );
-    // link.addEventListener(
-    //   "mouseleave",
-    //   function (event) {
-    //     // hoverSound.pause();
-    //   },
-    //   false
-    // );
-  }
-});
