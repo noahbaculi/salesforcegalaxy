e94722857ef391a1276184fb52f17cd69b66e61d
Noah <nbaculi@hotmail.com>
2021-09-30 12:25:28 -0700

    Cleaned up index.js removing deprecated code and adding comments.

--- automations.js
+++ automations.js
@@ -1,19 +1,24 @@
-// Noah Baculi Q3 2021
+// Noah Baculi 2021
 
-import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls';
+import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls";
 
 // Setup
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1500);
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1500
+);
 camera.position.setX(-300);
 camera.position.setY(100);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-  antialias: true
+  canvas: document.querySelector("#bg"),
+  antialias: true,
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -22,82 +27,96 @@ renderer.render(scene, camera);
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
-
 // Mouse interaction
 
 let lastMouseDownTime = 0;
 
 function calcIntersection(event) {
   // calculate mouse position in normalized device coordinates (-1 to +1) for both components
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
   // update the picking ray with the camera and mouse position
-	raycaster.setFromCamera(mouse, camera);
+  raycaster.setFromCamera(mouse, camera);
 
   // identify the intersection of the closest object that intersects the cursor
-	const intersection = raycaster.intersectObjects(scene.children)[0];
+  const intersection = raycaster.intersectObjects(scene.children)[0];
 
-  return intersection
+  return intersection;
 }
 
-let isMouseDown = false;  // tracks whether the mouse button is down to prevent hover behavior when the camera orbit is being controlled
+let isMouseDown = false; // tracks whether the mouse button is down to prevent hover behavior when the camera orbit is being controlled
 
 function onMouseMove(event) {
   // revert all hoverable objects to the unhovered state
   allHoverableObjects.forEach((hoverableObject) => {
-    hoverableObject.material.forEach((element) => { element.opacity = 1; })
-  })
+    hoverableObject.material.forEach((element) => {
+      element.opacity = 1;
+    });
+  });
 
-	const intersection = calcIntersection(event);
-  if (typeof intersection !== 'undefined' && typeof intersection.object.userData.length !== 'undefined' && !isMouseDown) {
+  const intersection = calcIntersection(event);
+  if (
+    typeof intersection !== "undefined" &&
+    typeof intersection.object.userData.length !== "undefined" &&
+    !isMouseDown
+  ) {
     // reduce the opacity for all the intersection object materials
-    intersection.object.material.forEach((element) => { element.opacity = 0.9; })
-    
-    document.body.style.cursor = 'pointer';
+    intersection.object.material.forEach((element) => {
+      element.opacity = 0.9;
+    });
+
+    document.body.style.cursor = "pointer";
+  } else {
+    document.body.style.cursor = "default";
   }
-  else { document.body.style.cursor = 'default'; }
 }
 
-window.addEventListener( 'mousemove', onMouseMove, false );
+window.addEventListener("mousemove", onMouseMove, false);
 
+function onMouseDown(event) {
+  const intersection = calcIntersection(event);
 
-function onMouseDown( event ) {
-	const intersection = calcIntersection(event);
-  
   // only process if there is an intersection and the intersection object has a
   // url in the userData attribute
-  if (typeof intersection !== 'undefined' && typeof intersection.object.userData.length !== 'undefined') {
+  if (
+    typeof intersection !== "undefined" &&
+    typeof intersection.object.userData.length !== "undefined"
+  ) {
     lastMouseDownTime = event.timeStamp;
   }
 
   isMouseDown = true;
 }
 
-window.addEventListener( 'mousedown', onMouseDown, false );
+window.addEventListener("mousedown", onMouseDown, false);
 
-function onMouseUp( event ) {
-	const intersection = calcIntersection(event);
-  
+function onMouseUp(event) {
+  const intersection = calcIntersection(event);
+
   // only process if there is an intersection, the intersection object has a
   // url in the userData attribute, and the last MouseDown event ocurred withinsome temporal
-	// threshold
+  // threshold
   const timeSinceMouseDown = event.timeStamp - lastMouseDownTime;
-  if (typeof intersection !== 'undefined' && typeof intersection.object.userData.length !== 'undefined' &&  timeSinceMouseDown < 200) {
-    window.open(intersection.object.userData, '_blank').focus();
+  if (
+    typeof intersection !== "undefined" &&
+    typeof intersection.object.userData.length !== "undefined" &&
+    timeSinceMouseDown < 200
+  ) {
+    window.open(intersection.object.userData, "_blank").focus();
 
     // revert all hoverable objects to the unhovered state
     allHoverableObjects.forEach((hoverableObject) => {
-    hoverableObject.material.forEach((element) => { element.opacity = 1; })
-  })
-
+      hoverableObject.material.forEach((element) => {
+        element.opacity = 1;
+      });
+    });
   }
 
   isMouseDown = false;
 }
 
-window.addEventListener( 'mouseup', onMouseUp, false );
-
+window.addEventListener("mouseup", onMouseUp, false);
 
 // Controls
 
@@ -106,16 +125,15 @@ controls.maxDistance = 400;
 
 // Background
 
-const backgroundTexture = new THREE.TextureLoader().load('data/grey.jpg');
+const backgroundTexture = new THREE.TextureLoader().load("data/grey.jpg");
 scene.background = backgroundTexture;
-
 
 // Pages
 
 const boxGeo = new THREE.BoxGeometry(1, 115, 80);
 
 function creatPageMaterials(frontImage, sfBack = true) {
-  const sidesTexture = new THREE.TextureLoader().load('data/pages/sides.jpg');
+  const sidesTexture = new THREE.TextureLoader().load("data/pages/sides.jpg");
   const sidesMaterial = new THREE.MeshBasicMaterial({ map: sidesTexture });
   sidesMaterial.transparent = true;
 
@@ -124,76 +142,100 @@ function creatPageMaterials(frontImage, sfBack = true) {
   frontMaterial.transparent = true;
 
   let backTexture, backMaterial;
-  if (sfBack) {  // if the page materials should include a back with the Salesforce logo
-    backTexture = new THREE.TextureLoader().load('data/pages/back.png');
+  if (sfBack) {
+    // if the page materials should include a back with the Salesforce logo
+    backTexture = new THREE.TextureLoader().load("data/pages/back.png");
     backMaterial = new THREE.MeshBasicMaterial({ map: backTexture });
-  }
-  else {
+  } else {
     backMaterial = sidesMaterial;
   }
   backMaterial.transparent = true;
 
-  return [backMaterial, frontMaterial, sidesMaterial, sidesMaterial, sidesMaterial, sidesMaterial];
+  return [
+    backMaterial,
+    frontMaterial,
+    sidesMaterial,
+    sidesMaterial,
+    sidesMaterial,
+    sidesMaterial,
+  ];
 }
-
 
 // Page box geometries
 
-const degToRad = 0.0174533;  // degrees to radians conversion factor
+const degToRad = 0.0174533; // degrees to radians conversion factor
 
-const box1 = new THREE.Mesh(boxGeo, creatPageMaterials('data/automations/validation_rules.png'));
+const box1 = new THREE.Mesh(
+  boxGeo,
+  creatPageMaterials("data/automations/validation_rules.png")
+);
 box1.position.set(-19, 0, -145);
 box1.rotation.set(0, 50 * degToRad, 0);
-box1.userData = 'https://help.salesforce.com/s/articleView?id=sf.fields_about_field_validation'
+box1.userData =
+  "https://help.salesforce.com/s/articleView?id=sf.fields_about_field_validation";
 scene.add(box1);
 
-const box2 = new THREE.Mesh(boxGeo, creatPageMaterials('data/automations/approvals.png'));
+const box2 = new THREE.Mesh(
+  boxGeo,
+  creatPageMaterials("data/automations/approvals.png")
+);
 box2.position.set(32, 0, -80);
 box2.rotation.set(0, 25 * degToRad, 0);
-box2.userData = 'https://help.salesforce.com/s/articleView?id=sf.what_are_approvals'
+box2.userData =
+  "https://help.salesforce.com/s/articleView?id=sf.what_are_approvals";
 scene.add(box2);
 
-const box3 = new THREE.Mesh(boxGeo, creatPageMaterials('data/automations/workflows.png'));
+const box3 = new THREE.Mesh(
+  boxGeo,
+  creatPageMaterials("data/automations/workflows.png")
+);
 box3.position.set(50, 0, 0);
-box3.userData = 'https://help.salesforce.com/s/articleView?id=sf.customize_wf'
+box3.userData = "https://help.salesforce.com/s/articleView?id=sf.customize_wf";
 scene.add(box3);
 
-const box4 = new THREE.Mesh(boxGeo, creatPageMaterials('data/automations/processes.png'));
+const box4 = new THREE.Mesh(
+  boxGeo,
+  creatPageMaterials("data/automations/processes.png")
+);
 box4.position.set(32, 0, 80);
 box4.rotation.set(0, -25 * degToRad, 0);
-box4.userData = 'https://help.salesforce.com/s/articleView?id=sf.process_overview'
+box4.userData =
+  "https://help.salesforce.com/s/articleView?id=sf.process_overview";
 scene.add(box4);
 
-const box5 = new THREE.Mesh(boxGeo, creatPageMaterials('data/automations/flows.png'));
+const box5 = new THREE.Mesh(
+  boxGeo,
+  creatPageMaterials("data/automations/flows.png")
+);
 box5.position.set(-19, 0, 145);
 box5.rotation.set(0, -50 * degToRad, 0);
-box5.userData = 'https://help.salesforce.com/s/articleView?id=sf.flow'
+box5.userData = "https://help.salesforce.com/s/articleView?id=sf.flow";
 scene.add(box5);
-
 
 // Credit board
 
 const creditBoxGeo = new THREE.BoxGeometry(1, 40, 80);
 const creditBoxMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-const creditBox = new THREE.Mesh(creditBoxGeo, creatPageMaterials('data/credit.png', false));
+const creditBox = new THREE.Mesh(
+  creditBoxGeo,
+  creatPageMaterials("data/credit.png", false)
+);
 creditBox.position.set(50, 180, 0);
 creditBox.rotation.set(0, 0, 50 * degToRad);
-creditBox.userData = 'https://noahbaculi.com'
+creditBox.userData = "https://noahbaculi.com";
 scene.add(creditBox);
 
-
-let allHoverableObjects = [box1, box2, box3, box4, box5, creditBox]
+let allHoverableObjects = [box1, box2, box3, box4, box5, creditBox];
 
 // Lights
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
 
-const pointLight1 = new THREE.PointLight(0xffffff);  // Point light to add shadows and interest to geometries
+const pointLight1 = new THREE.PointLight(0xffffff); // Point light to add shadows and interest to geometries
 pointLight1.position.set(-250, 70, 300);
-const lightHelper1 = new THREE.PointLightHelper(pointLight1)
+const lightHelper1 = new THREE.PointLightHelper(pointLight1);
 scene.add(pointLight1, lightHelper1);
-
 
 // Stars
 
@@ -202,7 +244,10 @@ function addStar() {
   const material = new THREE.MeshStandardMaterial({ color: 0xfaa73c });
   const star = new THREE.Mesh(geometry, material);
 
-  let distance = 0, x, y, z;
+  let distance = 0,
+    x,
+    y,
+    z;
 
   // do not allow stars closer than or farther than some distances
   while (distance < 260 || 500 < distance) {
@@ -217,15 +262,18 @@ function addStar() {
 
 const stars = [...Array(3000)].map(() => addStar());
 
-
 // Animation Loop
 
-const rotationAxis = new THREE.Vector3( 1, 2, -1 );
+const rotationAxis = new THREE.Vector3(1, 2, -1);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  stars.forEach(element => (element instanceof THREE.Mesh) ? element.position.applyAxisAngle(rotationAxis, 0.002 * degToRad) : element);
+  stars.forEach((element) =>
+    element instanceof THREE.Mesh
+      ? element.position.applyAxisAngle(rotationAxis, 0.002 * degToRad)
+      : element
+  );
 
   controls.update();
 
@@ -234,10 +282,9 @@ function animate() {
 
 animate();
 
-
 // Rsponsive Window Resizing
 
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener("resize", onWindowResize, false);
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
--- index.js
+++ index.js
@@ -1,2 +1,182 @@
-var vid = document.getElementById("bg-video");
-vid.playbackRate = 0.1;
+// Noah Baculi 2021
+
+import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
+
+// 
+// Setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1500
+);
+
+camera.position.z = 950; // move camera back to see cloud and stars
+camera.position.y = window.innerHeight / 5;
+camera.position.x = window.innerWidth / 5;
+
+camera.rotation.x = -0.3; // angle the camera down slightly so stars move past towards the bottom
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+  antialias: true,
+  alpha: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// 
+// Lighting
+
+const mainColor = 0x1e0329;
+scene.fog = new THREE.FogExp2(mainColor, 0.001);
+
+const ambient = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambient);
+
+let directionalLight = new THREE.DirectionalLight(0xff8c19);
+directionalLight.position.set(0, 0, 1);
+scene.add(directionalLight);
+
+let purpleLight = new THREE.PointLight(0x6035cc, 30, 500, 2);
+purpleLight.position.set(200, 300, 100);
+scene.add(purpleLight);
+let redLight = new THREE.PointLight(0xd8547e, 50, 500, 2);
+redLight.position.set(100, 300, 100);
+scene.add(redLight);
+let blueLight = new THREE.PointLight(0x3677ac, 40, 500, 2);
+blueLight.position.set(300, 300, 200);
+scene.add(blueLight);
+
+// 
+// Cloud texture
+
+let cloudParticles = [];
+
+let loader = new THREE.TextureLoader();
+loader.load("data/smoke.png", function (texture) {
+  const cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+  const cloudMaterial = new THREE.MeshLambertMaterial({
+    map: texture,
+    transparent: true,
+  });
+
+  for (let p = 0; p < 50; p++) {
+    let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+
+    let [x, y, z] = [
+      Math.random() * 500 - 250,
+      Math.random() * 500 - 250,
+      Math.random() * 500 - 250,
+    ];
+    cloud.position.set(x, y, z);
+    cloud.rotation.x = -0.3;
+    cloud.rotation.z = Math.random() * 2 * Math.PI;
+    cloud.material.opacity = 0.55;
+    cloudParticles.push(cloud);
+    scene.add(cloud);
+  }
+});
+
+//
+// Stars
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.08, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  let distance = 0,
+    x,
+    y,
+    z;
+
+  // do not allow stars closer than or farther than some distances
+  while (distance < 10 || 100 < distance) {
+    [x, y, z] = [...Array(3)].map(() => (Math.random() - 0.5) * 500);
+    distance = Math.hypot(x, y, z);
+  }
+
+  x += camera.position.x;
+  y += camera.position.y;
+  z += camera.position.z;
+
+  star.position.set(x, y, z);
+  scene.add(star);
+  return star;
+}
+
+const numStars = 800;
+const stars = [...Array(numStars)].map(() => addStar());
+
+//
+// Animation loop
+
+const rotationAxis = new THREE.Vector3(1, 2, -1);
+
+function animate() {
+  // rotate the cloud
+  cloudParticles.forEach((p) => {
+    p.rotation.z -= 0.0008;
+  });
+
+  // move the stars toward the camera
+  stars.forEach((star) => {
+    if (star instanceof THREE.Mesh) {
+      star.position.z += 0.1;
+
+      // jump the stars back to in front of the camera when they move behind the
+      // camera
+      if (star.position.z > camera.position.z) {
+        star.position.z -= 200;
+      }
+    }
+  });
+
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
+
+// 
+// Rsponsive Window Resizing
+
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+// 
+// HTML links hover sound effect
+
+const gridElements = document.getElementsByClassName("grid")[0];
+const links = Array.prototype.slice.call(gridElements.children);
+// console.log(links);
+
+links.forEach((link) => {
+  console.log(link.localName);
+  if (link.localName === "a") {
+    const hoverSound = new Audio("data/spaceboom.wav"); // buffers automatically when created
+    link.addEventListener(
+      "mouseenter",
+      function (event) {
+        console.log("mouseenter");
+        hoverSound.play();
+      },
+      false
+    );
+    // link.addEventListener(
+    //   "mouseleave",
+    //   function (event) {
+    //     // hoverSound.pause();
+    //   },
+    //   false
+    // );
+  }
+});
