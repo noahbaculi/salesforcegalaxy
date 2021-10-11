bd65c7e4f6db1fec78529d2f4bde9f463bfffb04
Noah <nbaculi@hotmail.com>
2021-10-10 20:17:48 -0700

    Added admin sales and marketing lesson.

---
+++ admin/sales-and-marketing-apps.js
@@ -0,0 +1,22 @@
+// Noah Baculi 2021
+
+import { generateLesson } from "/functions.js";
+
+// Map of lesson page data
+
+const lessonData = new Map([
+  [
+    "/data/admin/sales-and-marketing-apps/sales.png",
+    "https://help.salesforce.com/s/articleView?id=sf.sales_core_turn_opptys_into_deals.htm",
+  ],
+  [
+    "/data/admin/sales-and-marketing-apps/opportunity_tools.png",
+    "https://help.salesforce.com/s/articleView?id=sf.einstein_sales_lead_insights.htm",
+  ],
+  [
+    "/data/admin/sales-and-marketing-apps/marketing.png",
+    "https://help.salesforce.com/s/articleView?id=sf.campaigns_def.htm",
+  ],
+]);
+
+generateLesson(lessonData);
