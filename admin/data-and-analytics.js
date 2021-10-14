ecbdd13a35b141628629965856b3c5a12737d19d
Noah <nbaculi@hotmail.com>
2021-10-14 16:05:14 -0700

    Added data and analytics lesson.[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dadmin

---
+++ admin/data-and-analytics.js
@@ -0,0 +1,26 @@
+// Noah Baculi 2021
+
+import { generateLesson } from "/functions.js";
+
+// Map of lesson page data
+
+const lessonData = new Map([
+  [
+    "/data/admin/data-and-analytics/data_management.png",
+    "https://trailhead.salesforce.com/content/learn/modules/lex_implementation_data_management",
+  ],
+  [
+    "/data/admin/data-and-analytics/data_validation.png",
+    "https://help.salesforce.com/s/articleView?id=sf.fields_about_field_validation.htm&type=5",
+  ],
+  [
+    "/data/admin/data-and-analytics/reports.png",
+    "https://help.salesforce.com/s/articleView?id=sf.rd_reports_overview.htm&type=5",
+  ],
+  [
+    "/data/admin/data-and-analytics/dashboards.png",
+    "https://help.salesforce.com/s/articleView?id=sf.rd_dashboards_overview.htm&type=5",
+  ],
+]);
+
+generateLesson(lessonData);
