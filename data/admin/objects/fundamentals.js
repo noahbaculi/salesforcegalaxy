0c390fd8a3c87d78f5f49a812bb4a966d10f2355
Noah <nbaculi@hotmail.com>
2021-10-15 10:43:25 -0700

    Updated admin objects lesson, added app builder data modeling lesson, and added app builder fundamentals lesson links.

--- data/admin/objects/fundamentals.js
+++ data/admin/objects/fundamentals.js
@@ -7,27 +7,27 @@ import { generateLesson } from "/functions.js";
 const lessonData = new Map([
   [
     "/data/admin/data-and-analytics/reports.png",
-    "https://help.salesforce.com/s/articleView?id=sf.rd_reports_overview.htm&type=5",
+    "",
   ],
   [
     "/data/admin/data-and-analytics/dashboards.png",
-    "https://help.salesforce.com/s/articleView?id=sf.rd_dashboards_overview.htm&type=5",
+    "",
   ],
   [
     "/data/admin/setup/user_and_security_controls.png",
-    "https://help.salesforce.com/s/articleView?id=sf.security_data_access_mgmt.htm&type=5",
+    "",
   ],
   [
     "/data/admin/setup/ui_settings.png",
-    "https://help.salesforce.com/s/articleView?id=sf.customize_ui_settings.htm&type=5",
+    "",
   ],
   [
     "/data/admin/productivity-and-collaboration/appexchange.png",
-    "https://trailhead.salesforce.com/en/content/learn/modules/appexchange_basics",
+    "",
   ],
   [
     "/data/admin/productivity-and-collaboration/chatter.png",
-    "https://help.salesforce.com/s/articleView?id=sf.collab_chatter_get_started.htm&type=5",
+    "",
   ],
 ]);
 
