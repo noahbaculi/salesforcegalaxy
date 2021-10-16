eeab600477b9bf1bf15ac1e9db501bfbab3d8041
Noah <nbaculi@hotmail.com>
2021-10-16 15:51:22 -0700

    Added app buider user interface lesson

---
+++ app-builder/user-interface.js
@@ -0,0 +1,22 @@
+// Noah Baculi 2021
+
+import { generateLesson } from "/functions.js";
+
+// Map of lesson page data
+
+const lessonData = new Map([
+  [
+    "/data/admin/productivity-and-collaboration/mobile_app.png",
+    "https://help.salesforce.com/s/articleView?id=sf.salesforce_app.htm&type=5",
+  ],
+  [
+    "/data/app-builder/user-interface/ui-customization.png",
+    "https://help.salesforce.com/s/articleView?id=sf.customize_ui_settings.htm&type=5",
+  ],
+  [
+    "/data/app-builder/user-interface/buttons-links-actions.png",
+    "https://help.salesforce.com/s/articleView?id=sf.working_with_buttons_links_actions.htm&type=5",
+  ],
+]);
+
+generateLesson(lessonData);
